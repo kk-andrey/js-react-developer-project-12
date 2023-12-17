e2ef83c8ecf44cc5597b5bbc560c06b43b50664c
Andrey <huntyangel666@gmail.com>
2023-12-18 01:21:31 +0300

    Chore: Code clearance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,13 @@
+import {useEffect} from "react";
+
 export const App = () => {
+
+    useEffect(() => {
+        fetch('/api/v1/data')
+            .then(res => res.json())
+            .then(data => console.log('data'))
+            .catch(e => console.log(e))
+    }, []);
+
     return <>Welcome to study project</>
 }
